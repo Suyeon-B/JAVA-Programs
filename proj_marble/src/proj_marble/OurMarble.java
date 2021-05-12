c64657f7590d58e1bca526a017565cdabc897df0
Suyeon-B <tndus4243@naver.com>
2021-05-13 08:39:37 +0900

    우리의 마블 게임
    본 과제는 클래스를 이해하기 위한 과제이다.
    – 따라서, 도시(City)와 플레이어(Player)를 반드시 객체로 만들어 사용해야 한다.
    
    우리의 마블은 세계 10개 도시를 여행하면서 부동산을 구입하고 통행료 를 받는 게임이다. 2명의 플레이어가 하는 게임이다.
    
    주사위(1-6까지만 적혀있는 주사위임)를 던져서 나온 수만큼 이동한다.
    
    플레이어는 5000원으로 게임을 시작한다.
    
    이 게임은 사람이 하는 것이 아니라 컴퓨터가 자동으로 2명을 플레이하는 것이다.
    
    화면 구성은 예쁘지 않아도 된다. 알아볼 수만 있으면 된다.

---
+++ proj_marble/src/proj_marble/OurMarble.java
@@ -0,0 +1,147 @@
+package proj_marble;
+import java.util.Scanner;
+
+class City {
+	String city;
+	int owner;
+	int price;
+	int toll;
+	
+	City(){ // 생성자 
+		String city = "";
+		String owner = "empty";
+		int price = 300;
+		int toll = 600;
+	}
+	
+	City(String city, int owner, int price, int toll){ // 매개변수가 있는 생성자 
+		this.city = city;
+		this.owner = owner;
+		this.price = price;
+		this.toll = toll;
+	}
+}
+
+class Player {
+	int player;
+	int budget;
+	int diceNum;
+	int diceSum;
+	String[] ownCities = new String[9]; // 도시목록: (Start), Seoul, Tokyo, Sydney, LA, Cairo, Phuket, New delhi, Hanoi, Paris
+	
+	Player(){
+		int player = 0;
+		int budget = 5000;
+		int diceNum = 0;
+		int diceSum = 0;
+		for (int i=0;i<9;i++) {
+			ownCities[i] = null;
+		}
+	}
+	
+	Player(int player, int budget, int diceNum, int diceSum, String[] ownCities){
+		this.player = player;
+		this.budget = budget;
+		this.diceNum = diceNum;
+		this.diceSum = diceSum;
+		this.ownCities = ownCities;
+	}
+	
+}
+
+
+class PlayGame {
+	Scanner scanner = new Scanner(System.in);
+	City[] cityList = new City[9];
+	Player[] playerList = new Player[2];
+	String[] Cities = {"Start", "Seoul", "Tokyo", "Sydney", "LA", "Cairo", "Phuket", "New delhi", "Hanoi", "Paris"};
+	
+	PlayGame() { 
+		// 생성자 : 플레이어의 순번 지정과 초기화 
+		for(int i=1;i<=9;i++) {
+			cityList[i-1] = new City(); 
+			cityList[i-1].city = Cities[i-1];
+		}
+		for(int i=1;i<=2;i++) {
+			playerList[i-1] = new Player(); 
+			playerList[i-1].player = i;
+		}
+		
+	}
+	
+	void showResult(int i) {
+		System.out.printf("%S(%d)\n", Cities[playerList[i].diceSum], cityList[playerList[i].diceSum].owner);
+		if (Cities[playerList[i].diceSum] == "empty"){
+			buyCity(i, playerList[i].diceSum);
+		}
+		else {
+			payToll(i, playerList[i].diceSum);
+		}
+			
+	}
+	
+	void randDiceNum(int i) { // 플레이어의 주사위 숫자 랜덤으로 뽑기 
+		playerList[i].diceNum = (int) (Math.random() * 6) +1;
+		playerList[i].diceSum += playerList[i].diceNum;
+			
+		System.out.printf("player %d: %d", i, playerList[i].diceNum);
+	}
+	
+	void buyCity(int player, int location) {
+		System.out.printf("%S(%d)\n", Cities[location], cityList[location].owner);
+		if (playerList[player].budget < cityList[location].price) {
+			System.out.println("자금이 부족합니다.");
+		}
+		else {
+			cityList[location].owner = playerList[player].player;
+		}
+	}
+	
+	void payToll(int player, int location){
+		if (cityList[location].owner != player) {
+			System.out.println("통행료를 지불하세요.");
+			// 통행료를 지불할 돈이 부족하면 게임 종료 
+			if (playerList[player].budget < cityList[location].toll) {
+				System.out.println("자금이 부족합니다. 탈락입니다.");
+				if (player == 1) {
+					System.out.printf("player %d의 승리입니다.\n", 2);
+				}
+				else {
+					System.out.printf("player %d의 승리입니다.\n", 1);
+				}
+			}
+			// 통행료 지불 
+			else {
+				playerList[player].budget -= 600;
+				playerList[2].budget += 600;
+				System.out.printf("player %d (남은 자금): ", player, +  playerList[player].budget);
+				if (player == 1) {
+					System.out.printf("player %d (남은 자금): ", 2, +  playerList[2].budget);
+				}
+				else {
+					System.out.printf("player %d (남은 자금): ", 1, +  playerList[1].budget);
+				}
+			}
+
+		}
+	}
+
+
+public class OurMarble {}
+	public static void main(String[] args) {
+		PlayGame game = new PlayGame();
+	
+		// 게임 시작
+		int turn = 30;
+		
+		while (turn > 0) {
+			// 주사위 뽑고 게임 시작 
+			for(int i=1;i<=2;i++) {
+				game.randDiceNum(i);
+				game.showResult(i);
+			}
+			turn -=1 ;
+		}
+	}
+
+}
